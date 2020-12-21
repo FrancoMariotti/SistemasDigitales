656f37f71b47ce4f6063ddc38d28c9b178493521
Franco Mariotti <fmariott@fi.uba.ar>
2020-12-21 00:48:32 -0300

    agrego resolucion ejercicio 7 guia 1

--- ejercicios/Guia1/Ejercicio7/Fuentes/multNb_tb.vhd
+++ ejercicios/Guia1/Ejercicio7/Fuentes/multNb_tb.vhd
@@ -21,13 +21,20 @@ architecture mulNb_tb_arq of mulNb_tb is
 	signal b_tb: std_logic_vector(N_tb-1 downto 0);
 	signal s_tb: std_logic_vector(2*N_tb-1 downto 0);
 begin 
-	--a_tb <= "00010" after 100 ns,"00001" after 200 ns;
-	--b_tb <= "00011" after 100 ns,"01000" after 200 ns;
+	--a_tb <= "0010", "0101" after 10 ns, "1111" after 20 ns, "0000" after 30 ns;
+	--b_tb <= "1001", "1110" after 10 ns, "1111" after 20 ns, "0000" after 30 ns;
 	
-	--tb_x0 <= "0010", "0101" after 10 ns, "1111" after 20 ns, "0000" after 30 ns;
-	--tb_x1 <= "1001", "1110" after 10 ns, "1111" after 20 ns, "0000" after 30 ns;
-	a_tb <= "0010", "0101" after 10 ns, "1111" after 20 ns, "0000" after 30 ns;
-	b_tb <= "1001", "1110" after 10 ns, "1111" after 20 ns, "0000" after 30 ns;
+	-- +2 +5 -1 -8
+	a_tb <= "0010", "0101" after 10 ns, "1111" after 20 ns, "1000" after 30 ns,
+	-- -8
+	"1000" after 40 ns, "0000" after 50 ns;
+	-- -7 -6 -1 +7
+	b_tb <= "1001", "1010" after 10 ns, "1111" after 20 ns, "0111" after 30 ns,
+	-- -8
+	"1000" after 40 ns, "0000" after 50 ns;
+	-- tb_y: -14 	-30 	+1 		-56 	-64
+	-- 		1110010 1100010 0000001 1001000 1000000
+	
 
 	DUT: mulNb
 	generic map(
