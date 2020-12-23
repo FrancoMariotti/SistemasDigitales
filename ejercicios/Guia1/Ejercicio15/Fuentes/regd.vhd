a64b3c6d1771a09e1f9de6d6d4dfcf543b248c21
Franco Mariotti <fmariott@fi.uba.ar>
2020-12-23 11:48:23 -0300

    agrego resolucion ejercicio 15 guia 1

--- ejercicios/Guia1/Ejercicio15/Fuentes/regd.vhd
+++ ejercicios/Guia1/Ejercicio15/Fuentes/regd.vhd
@@ -14,18 +14,21 @@ entity regd is
 end;
 
 architecture regd_arq of regd is
+	signal aux: std_logic_vector(N-1 downto 0);
 begin
-	process (clk_i,load_i) is
+	process (clk_i,rst_i,load_i) is	
 	begin 
 		if rising_edge(clk_i) then
-			if	rst_i = '1' then 
-				Q_o <= (others => '0');
-			else if load_i = '1' then
-				Q_o <= value_i;
+			if rst_i = '1' then 
+				aux <= (others => '0');
+			elsif load_i = '1' then
+				aux <= value_i;
 			else
-				Q_o(N-1 downto 1) <= aux(N-2 downto 0);
-				Q_o(0) <= D_i;
+				aux(N-1 downto 1) <= aux(N-2 downto 0);
+				aux(0) <= D_i;
 			end if;
 		end if;
 	end process;
+	
+	Q_o <= aux;
 end regd_arq;
--- ejercicios/Guia1/Ejercicio15/Fuentes/regd_tb.vhd
+++ ejercicios/Guia1/Ejercicio15/Fuentes/regd_tb.vhd
@@ -22,13 +22,14 @@ architecture regd_tb_arq of regd_tb is
 	signal clk_tb:		std_logic := '0';
 	signal rst_tb:		std_logic := '0';
 	signal D_tb:		std_logic := '0';
-	signal Q_tb: 		std_logic_vector(N-1 downto 0);
 	signal load_tb:		std_logic := '0';
-	signal value_tb: 	std_logic_vector(N-1 downto 0) := "0010";
+	signal value_tb: 	std_logic_vector(N_tb-1 downto 0) := "0010";
+	signal Q_tb: 		std_logic_vector(N_tb-1 downto 0);
 begin
 	D_tb 	<=	not D_tb after 20 ns;
-	rst_tb 	<= '1', '0' after 10 ns;
+	rst_tb 	<= 	'1', '0' after 10 ns;
 	clk_tb 	<=	not clk_tb after 5 ns;
+	load_tb	<=	'0', '1' after 60 ns, '0' after 70 ns;
 	
 	DUT: regd
 		generic map(
@@ -40,6 +41,6 @@ begin
 			D_i		=>	D_tb,
 			load_i 	=> 	load_tb,
 			value_i	=> 	value_tb,
-			Q _o	=> 	Q_tb
+			Q_o		=> 	Q_tb
 		);
 end regd_tb_arq;
