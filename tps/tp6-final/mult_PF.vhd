675a24c5dc5399a359cfa3403707f68b7e1f14b7
Franco Mariotti <fmariotti@fi.uba.ar>
2021-01-02 19:30:50 -0300

    agrego logica a la descripcion del componente

--- tps/tp6-final/mult_PF.vhd
+++ tps/tp6-final/mult_PF.vhd
@@ -15,6 +15,8 @@ entity mult_PF is
 end entity;
 
 architecture behavioral of mult_PF is
+
+	--declaracion multiplicador.
 	component multiplicador is
 		generic(
 			N: natural := 4
@@ -28,13 +30,22 @@ architecture behavioral of mult_PF is
 		);
 	end component;
 
-	constant SIGNIFICAND_SIZE: natural := WORD_SIZE - EXP_SIZE;
+	constant SIGNIFICAND_SIZE:	natural := WORD_SIZE - EXP_SIZE;
+	constant MANTISA_SIZE:		natural := SIGNIFICAND_SIZE-1;
+	constant RESULT_SIZE:		natural := 2*SIGNIFICAND_SIZE;
+
+	signal x0:					std_logic;
+	signal exponentResult:		std_logic_vector(EXP_SIZE-1 downto 0);	
 
 	signal significandA:		std_logic_vector(SIGNIFICAND_SIZE-1 downto 0);
 	signal significandB:		std_logic_vector(SIGNIFICAND_SIZE-1 downto 0);
-	signal significandResult:	std_logic_vector(SIGNIFICAND_SIZE-1 downto 0);
+	signal significandResult:	std_logic_vector(RESULT_SIZE downto 0);
 begin
-
+	
+	significandA <= '1' & a_i(MANTISA_SIZE-1 downto 0);
+	significandB <= '1' & b_i(MANTISA_SIZE-1 downto 0);
+	
+	-- instancia multiplicador para enteros sin signo.
 	mult_inst: multiplicador
 		generic map(
 			N => SIGNIFICAND_SIZE
@@ -47,5 +58,13 @@ begin
 			product_o	=> significandResult
 		);
 
+	--falta tener en cuenta que el multiplicador tarda N ciclos en obtener el resultado.
+	--bit mas significativo del resultado de significandA * significandB.
+	x0 <= significandResult(RESULT_SIZE-1);
+
+	s_o(MANTISA_SIZE-1 downto 0) <=	significandResult(RESULT_SIZE-2 downto SIGNIFICAND_SIZE) when x0 = '1' else
+									else significandResult(RESULT_SIZE-3 downto SIGNIFICAND_SIZE+1)
+														
+	-- signo del resultado.
 	s_o(WORD_SIZE-1) <= a_i(WORD_SIZE-1) xor b_i(WORD_SIZE-1);
 end architecture ;
