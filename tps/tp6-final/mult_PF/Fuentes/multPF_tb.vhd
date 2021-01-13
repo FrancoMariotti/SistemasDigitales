743d4a05a5b5363fd3a2a22cfb026b325308c570
Franco Mariotti <fmariotti@fi.uba.ar>
2021-01-13 15:57:38 -0300

    refactor componente multiplicador punto flotante

--- tps/tp6-final/mult_PF/Fuentes/multPF_tb.vhd
+++ tps/tp6-final/mult_PF/Fuentes/multPF_tb.vhd
@@ -1,11 +1,34 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use std.textio.all;
 
 entity multPF_tb is
 end;
 
 architecture multPF_tb_arq of multPF_tb is
+	constant SIM_TIME_NS: time := 800 ns;
+	constant TCK: time := 30 ns; 		-- periodo de reloj
+	constant DELAY: natural:= 0; 		-- retardo de procesamiento del DUT
+	constant WORD_SIZE_T:	natural	:=	23;
+	constant EXP_SIZE_T:	natural	:=	6;
+
+	signal	clk_tb:		std_logic := '0';
+	signal a_file: unsigned(WORD_SIZE_T-1 downto 0):= (others => '0');
+	signal b_file: unsigned(WORD_SIZE_T-1 downto 0):= (others => '0');
+	signal z_file: unsigned(WORD_SIZE_T-1 downto 0):= (others => '0');
+	signal z_del: unsigned(WORD_SIZE_T-1 downto 0):= (others => '0');
+	signal z_dut: unsigned(WORD_SIZE_T-1 downto 0):= (others => '0');
+	
+	signal ciclos: integer := 0;
+	signal errores: integer := 0;
+
+	-- La senal z_del_aux se define por un problema de conversión
+	signal z_del_aux: std_logic_vector(WORD_SIZE_T-1 downto 0):= (others => '0');
+	
+	file datos: text open read_mode is "../../test_files/test_mul_float_23_6.txt";
+
+
 	--declaracion de componente multiplicador de punto flotante.
 	component mult_PF is
 		generic(
@@ -13,52 +36,108 @@ architecture multPF_tb_arq of multPF_tb is
 			EXP_SIZE:	natural := 6
 		);
 		port(
-			clk_i:		in std_logic;
-			start_i:	in std_logic;
 			a_i  :		in std_logic_vector(WORD_SIZE-1 downto 0);
 			b_i  :		in std_logic_vector(WORD_SIZE-1 downto 0);
 			s_o  :		out std_logic_vector(WORD_SIZE-1 downto 0)
 		);
 	end component;
 
+	--Declaracion de la linea de retardo
+	component delay_gen is
+		generic(
+			N: natural:= 26;
+			DELAY: natural:= 0
+		);
+		port(
+			clk: in std_logic;
+			A: in std_logic_vector(N-1 downto 0);
+			B: out std_logic_vector(N-1 downto 0)
+		);
+	end component;
 
-	constant SIM_TIME_NS: time := 800 ns;
-	constant TCK: time := 20 ns; 		-- periodo de reloj
-	constant WORD_SIZE_T:	natural	:=	23;
-	constant EXP_SIZE_T:	natural	:=	6;
-
-	signal	clk_tb:		std_logic := '0';
-	signal	start_tb:	std_logic;
-	signal	a_tb:		std_logic_vector(WORD_SIZE_T-1 downto 0);
-	signal	b_tb:		std_logic_vector(WORD_SIZE_T-1 downto 0);
-	signal	s_tb:		std_logic_vector(WORD_SIZE_T-1 downto 0);
+	--signal	a_tb:		std_logic_vector(WORD_SIZE_T-1 downto 0);
+	--signal	b_tb:		std_logic_vector(WORD_SIZE_T-1 downto 0);
+	--signal	s_tb:		std_logic_vector(WORD_SIZE_T-1 downto 0);
 	
 begin
 	
 	clk_tb		<= not(clk_tb) after TCK/ 2;
-	a_tb		<= std_logic_vector(to_unsigned(8323071,WORD_SIZE_T));
-	b_tb		<= std_logic_vector(to_unsigned(2007961,WORD_SIZE_T));
-	start_tb	<= '0','1' after 10 ns;
+	--a_tb		<= std_logic_vector(to_unsigned(8320344,WORD_SIZE_T)), std_logic_vector(to_unsigned(968047,WORD_SIZE_T)) after 100 ns;
+	--b_tb		<= std_logic_vector(to_unsigned(6298089,WORD_SIZE_T)), std_logic_vector(to_unsigned(4951893,WORD_SIZE_T)) after 100 ns,std_logic_vector(to_unsigned(4951893,WORD_SIZE_T)) after 200 ns;
 
-	--instancia multiplicador de punto flotante.
+	Test_Sequence: process
+		variable l: line;
+		variable ch: character:= ' ';
+		variable aux: integer;
+	begin
+		while not(endfile(datos)) loop 		-- si se quiere leer de stdin se pone "input"
+			wait until rising_edge(clk_tb);
+			ciclos <= ciclos + 1;			-- solo para debugging
+			readline(datos, l); 			-- se lee una linea del archivo de valores de prueba
+			read(l, aux); 					-- se extrae un entero de la linea
+			a_file <= to_unsigned(aux, WORD_SIZE_T); 	-- se carga el valor del operando A
+			read(l, ch); 					-- se lee un caracter (es el espacio)
+			read(l, aux); 					-- se lee otro entero de la linea
+			b_file <= to_unsigned(aux, WORD_SIZE_T); 	-- se carga el valor del operando B
+			read(l, ch); 					-- se lee otro caracter (es el espacio)
+			read(l, aux); 					-- se lee otro entero
+			z_file <= to_unsigned(aux, WORD_SIZE_T); 	-- se carga el valor de salida (resultado)
+		end loop;
+		
+		file_close(datos);		-- se cierra del archivo
+		wait for TCK*(DELAY+1);
+		assert false report		-- se aborta la simulacion (fin del archivo)
+			"Fin de la simulacion" severity failure;
+	end process Test_Sequence;
+
+	--Instanciacion multiplicador de punto flotante.
 	DUT: mult_PF
 		generic map(
 			WORD_SIZE	=> WORD_SIZE_T,
 			EXP_SIZE	=> EXP_SIZE_T
 		)
 		port map(
-			clk_i	=> clk_tb,
-			start_i => start_tb,
-			a_i		=> a_tb,
-			b_i		=> b_tb,
-			s_o 	=> s_tb
+			--a_i		=> std_logic_vector(a_tb),
+			--b_i		=> std_logic_vector(b_tb),
+			--s_o 	=> s_tb
+			a_i		=> std_logic_vector(a_file),
+			b_i		=> std_logic_vector(b_file),
+			unsigned(s_o) 	=> z_dut
 		);
 
-	stop_simulation : process
-		begin
-			wait for SIM_TIME_NS; --run the simulation for this duration
-			assert false
-			report "Simulation finished."
-			severity failure;
-		end process;
+
+	 --Instanciacion de la linea de retardo
+	del: delay_gen
+			generic map(WORD_SIZE_T, DELAY)
+			port map(clk_tb, std_logic_vector(z_file), z_del_aux);
+				
+	z_del <= unsigned(z_del_aux);
+	
+	-- Verificacion de la condicion
+	verificacion: process(clk_tb)
+	begin
+		if rising_edge(clk_tb) then
+			--report integer'image(to_integer(a_file)) & " " & integer'image(to_integer(b_file)) & " " & integer'image(to_integer(z_file));
+			assert to_integer(z_del) = to_integer(z_dut) report
+				"Error: Salida del DUT no coincide con referencia (salida del dut = " & 
+				integer'image(to_integer(z_dut)) &
+				", salida del archivo = " &
+				integer'image(to_integer(z_del)) & ")" &
+				" " & integer'image(to_integer(a_file)) 
+				& " " & integer'image(to_integer(b_file)) 
+				& " " & integer'image(to_integer(z_file))
+				severity warning;
+			if to_integer(z_del) /= to_integer(z_dut) then
+				errores <= errores + 1;
+			end if;
+		end if;
+	end process;
+
+	--stop_simulation : process
+	--	begin
+	--		wait for SIM_TIME_NS; --run the simulation for this duration
+	--		assert false
+	--		report "Simulation finished."
+	--		severity failure;
+	--	end process;
 end architecture;
--- tps/tp6-final/mult_PF/Fuentes/mult_PF.vhd
+++ tps/tp6-final/mult_PF/Fuentes/mult_PF.vhd
@@ -9,8 +9,6 @@ entity mult_PF is
 		EXP_SIZE:	natural := 6
 	);
 	port(
-		clk_i:		in std_logic;
-		start_i:	in std_logic;
 		a_i  :		in std_logic_vector(WORD_SIZE-1 downto 0);
 		b_i  :		in std_logic_vector(WORD_SIZE-1 downto 0);
 		s_o  :		out std_logic_vector(WORD_SIZE-1 downto 0)
@@ -18,6 +16,19 @@ entity mult_PF is
 end entity;
 
 architecture behavioral of mult_PF is
+	constant N_SIGNIF:			natural := WORD_SIZE - EXP_SIZE;	--tamanio de los significAND
+	constant N_MANT:			natural := N_SIGNIF-1;				--tamanio de la mantisa
+	constant N_PROD:			natural := 2*N_SIGNIF;				--tamanio del producto de significANDs
+
+	constant BIAS:				std_logic_vector(EXP_SIZE-1 downto 0)  := '0' & (EXP_SIZE-2 downto 0 => '1');	  --bias
+	constant MINUS_BIAS:		std_logic_vector(EXP_SIZE-1 downto 0)  := std_logic_vector(signed(not bias) + 1); --complemento bias
+
+	constant ZERO_EXP:			std_logic_vector(EXP_SIZE-1 downto 0)  := (others => '0'); 
+	constant ONE_EXP:			std_logic_vector(EXP_SIZE-1 downto 0)  := (EXP_SIZE-1 downto 1 => '0') & '1'; 
+	constant MAX_EXP:			std_logic_vector(EXP_SIZE-1 downto 0)  := (EXP_SIZE-1 downto 1 => '1') & '0'; 
+
+	constant ZERO_MANT:			std_logic_vector(N_MANT-1 downto 0)  := (others => '0'); 
+	constant MAX_MANT:			std_logic_vector(N_MANT-1 downto 0)  := (N_MANT-1 downto 0 => '1'); 
 
 	--declaracion sumador generico de N bits.
 	component sumNb is
@@ -34,53 +45,89 @@ architecture behavioral of mult_PF is
 	end component;
 
 	--declaracion multiplicador.
-	component multiplicador is
+	component multNb is
 		generic(
 			N: natural := 4
 		);
 		port(
-			clk_i    : in std_logic;
-			load_i   : in std_logic;
-			opA_i    : in std_logic_vector(N-1 downto 0);
-			opB_i    : in std_logic_vector(N-1 downto 0);
-			done_o   : out std_logic;
-			product_o: out std_logic_vector(2*N-1 downto 0)
+			multiplicAND: in std_logic_vector(N-1 downto 0);
+			multiplier:	in std_logic_vector(N-1 downto 0);
+			result:	out std_logic_vector(2*N-1 downto 0)
 		);
 	end component;
-
-	constant Nsignif:			natural := WORD_SIZE - EXP_SIZE;	--tamanio de los significand
-	constant Nmant:				natural := Nsignif-1;				--tamanio de la mantisa
-	constant Nprod:				natural := 2*Nsignif;				--tamanio del producto de significands
-
-	signal pN:					std_logic;
-	signal bias:				std_logic_vector(EXP_SIZE-1 downto 0) := '0' & (EXP_SIZE-2 downto 0 => '1');--std_logic_vector(to_signed(-((2**EXP_SIZE)-1),EXP_SIZE))'';
-	signal minusBias:			std_logic_vector(EXP_SIZE-1 downto 0);
-
-	signal expA:				std_logic_vector(EXP_SIZE-1 downto 0);
-	signal expB:				std_logic_vector(EXP_SIZE-1 downto 0);
-	signal salSumExp:			std_logic_vector(EXP_SIZE-1 downto 0);
-	signal coSumExp:			std_logic;
 	
-	signal load_i:				std_logic;
-	signal salSumBias:			std_logic_vector(EXP_SIZE-1 downto 0);
+	signal negToNeg:			std_logic;							    
+	signal posToNeg:			std_logic;							    
+	signal negToPos:			std_logic;							    
 
-	signal entSumInc:			std_logic_vector(EXP_SIZE-1 downto 0);
-	signal salSumInc:			std_logic_vector(EXP_SIZE-1 downto 0);
+	signal resMultMsb:			std_logic;							    --bit mas significativo de la salida del multiplicador
+	signal underflow:			std_logic;							    --bit de underflow
+	signal overflow:			std_logic;							    --bit de overflow
 
-	signal significandA:		std_logic_vector(Nsignif-1 downto 0);
-	signal significandB:		std_logic_vector(Nsignif-1 downto 0);
-	signal salMult:				std_logic_vector(Nprod-1 downto 0);
+	signal expA:				std_logic_vector(EXP_SIZE-1 downto 0);	--exponente operador A
+	signal expB:				std_logic_vector(EXP_SIZE-1 downto 0);	--exponente operador B
+	signal salSumExp:			std_logic_vector(EXP_SIZE-1 downto 0);	--salida sumador de exponentes
+
+	signal salSumBias:			std_logic_vector(EXP_SIZE-1 downto 0);	--salida sumador de bias
+
+	signal entSumInc:			std_logic_vector(EXP_SIZE-1 downto 0);	--entrada incrementador exponente
+	signal salSumInc:			std_logic_vector(EXP_SIZE-1 downto 0);	--salida incrementador exponente
+
+	signal significandA:		std_logic_vector(N_SIGNIF-1 downto 0);	--significAND operador A
+	signal significandB:		std_logic_vector(N_SIGNIF-1 downto 0);	--significAND operador B
+	signal salMult:				std_logic_vector(N_PROD-1 downto 0);	--salida multiplicador de significANDs
+	
+	signal salRestBiasA:		std_logic_vector(EXP_SIZE-1 downto 0);	--salida restador de bias del operador A
+	signal salRestBiasB:		std_logic_vector(EXP_SIZE-1 downto 0);	--salida restador de bias del operador B
+
+	signal salMuxMult:			std_logic_vector(N_MANT-1 downto 0);    --salida del multiplexor que conecta la salida del multiplicador.
+	signal salMuxMantisa:		std_logic_vector(N_MANT-1 downto 0);    --salida del multiplexor que conecta la mantisa a la salida.
+
+	signal salMuxExp:			std_logic_vector(EXP_SIZE-1 downto 0);
+
+
+	function is_all(vector: std_logic_vector; value: std_logic) return std_logic is
+		constant aux: std_logic_vector(vector'range) := (others => value);
+	begin
+		if vector = aux  then 
+			return '1';
+		end if;
+
+		return '0';  
+	end function;
+
 begin
-	
-	load_i <= not start_i;
+	expA <= a_i(WORD_SIZE-2 downto N_MANT);
+	expB <= b_i(WORD_SIZE-2 downto N_MANT);
 
-	expA <= a_i(WORD_SIZE-2 downto Nmant);
-	expB <= b_i(WORD_SIZE-2 downto Nmant);
+	significandA <= '1' & a_i(N_MANT-1 downto 0);
+	significandB <= '1' & b_i(N_MANT-1 downto 0);
 
-	significandA <= '1' & a_i(Nmant-1 downto 0);
-	significandB <= '1' & b_i(Nmant-1 downto 0);
+	---- instancia sumador de BIAS complementado
+	restador_biasA: sumNb
+		generic map(
+			N => EXP_SIZE
+		)
+		port map(
+			a_i		=> MINUS_BIAS,
+			b_i		=> expA,
+			ci_i	=> '0',
+			s_o		=> salRestBiasA,
+			co_o	=> open
+		);
 
-	minusBias <= std_logic_vector(signed(not bias) + 1);
+	---- instancia sumador de BIAS complementado
+	restador_biasB: sumNb
+		generic map(
+			N => EXP_SIZE
+		)
+		port map(
+			a_i		=> MINUS_BIAS,
+			b_i		=> expB,
+			ci_i	=> '0',
+			s_o		=> salRestBiasB,
+			co_o	=> open
+		);
 
 	-- instancia sumador de exponentes
 	sumador_exp: sumNb
@@ -88,24 +135,10 @@ begin
 			N => EXP_SIZE
 		)
 		port map(
-			a_i		=> expA,
-			b_i		=> expB,
+			a_i		=> salRestBiasA,
+			b_i		=> salRestBiasB,
 			ci_i	=> '0',
 			s_o		=> salSumExp,
-			co_o	=> coSumExp
-		);
-
-
-	-- instancia sumador de bias
-	sumador_bias: sumNb
-		generic map(
-			N => EXP_SIZE
-		)
-		port map(
-			a_i		=> minusBias,
-			b_i		=> salSumExp,
-			ci_i	=> '0',
-			s_o		=> salSumBias,
 			co_o	=> open
 		);
 
@@ -115,40 +148,74 @@ begin
 			N => EXP_SIZE
 		)
 		port map(
-			a_i 	=> salSumBias,
+			a_i 	=> salSumExp,
 			b_i		=> entSumInc,
 			ci_i	=> '0',
 			s_o		=> salSumInc,
 			co_o	=> open
 		);
-	
-	-- instancia multiplicador para enteros sin signo.
-	mult_inst: multiplicador
+
+	-- instancia sumador de BIAS	
+	sumador_bias: sumNb
 		generic map(
-			N => Nsignif
+			N => EXP_SIZE
 		)
 		port map(
-			clk_i		=> clk_i, 
-			load_i		=> load_i,
-			opA_i		=> significandA,
-			opB_i		=> significandB,
-			done_o		=> open,
-			product_o	=> salMult
+			a_i		=> salSumInc,
+			b_i		=> bias,
+			ci_i	=> '0',
+			s_o		=> salSumBias,
+			co_o	=> open
 		);
 
 	--bit mas significativo del producto significandA * significandB.
-	pN <= salMult(Nprod-1);
+	resMultMsb <= salMult(N_PROD-1);
+
+	entSumInc <= ONE_EXP when resMultMsb = '1' else ZERO_EXP;
 	
-	entSumInc <= std_logic_vector(to_unsigned(1,EXP_SIZE)) when pN = '1' else 
-				 std_logic_vector(to_unsigned(0,EXP_SIZE));
+	-- instancia multiplicador para enteros sin signo.
+	mult_inst: multNb
+		generic map(
+			N => N_SIGNIF
+		)
+		port map(
+			multiplicAND	=> significandA,
+			multiplier		=> significandB,
+			result			=> salMult
+		);
+
+	negToPos <= salRestBiasA(EXP_SIZE-1) AND salRestBiasB(EXP_SIZE-1) AND NOT(salSumExp(EXP_SIZE-1)); 
+	posToNeg <= NOT(salRestBiasA(EXP_SIZE-1)) AND NOT(salRestBiasB(EXP_SIZE-1)) AND salSumExp(EXP_SIZE-1);
+	negToNeg <= salRestBiasA(EXP_SIZE-1) AND salRestBiasB(EXP_SIZE-1) AND salSumExp(EXP_SIZE-1); 
+	
+	--logica underflow.
+	underflow <= negToPos OR 
+				 (negToNeg AND salSumExp(EXP_SIZE-1) AND is_all(salSumExp(EXP_SIZE-2 downto 1),'0') AND 
+				 	(NOT(salSumExp(0)) OR (salSumExp(0) AND NOT(resMultMsb))));
+
+	--logica overflow.
+	overflow <= posToNeg OR
+				(resMultMsb AND NOT(salSumExp(EXP_SIZE-1)) AND is_all(salSumExp(EXP_SIZE-2 downto 0),'1'));
+
+
+	salMuxExp <= ZERO_EXP when (overflow = '0' AND underflow ='1') else
+				 MAX_EXP  when (overflow = '1' AND underflow ='0') else
+				 salSumBias;
+
+	salMuxMult <= salMult(N_PROD-2 downto N_SIGNIF) when resMultMsb = '1' else 
+				  salMult(N_PROD-3 downto N_SIGNIF-1);
+
+	salMuxMantisa <= ZERO_MANT when (overflow = '0' AND underflow ='1') else
+					 MAX_MANT  when (overflow = '1' AND underflow ='0') else
+					 salMuxMult;
 
 	--mantisa del resultado.
-	s_o(Nmant-1 downto 0) <= salMult(Nprod-2 downto Nsignif) when pN = '1' else 
-							 salMult(Nprod-3 downto Nsignif-1);
+	s_o(N_MANT-1 downto 0) <= salMuxMantisa;
 
-	--exponente del resultado.
-	s_o(WORD_SIZE-2 downto Nmant) <= salSumInc;
-														
+	----exponente del resultado.
+	s_o(WORD_SIZE-2 downto N_MANT) <= salMuxExp;
+
 	-- signo del resultado.
 	s_o(WORD_SIZE-1) <= a_i(WORD_SIZE-1) xor b_i(WORD_SIZE-1);
+
 end architecture;
